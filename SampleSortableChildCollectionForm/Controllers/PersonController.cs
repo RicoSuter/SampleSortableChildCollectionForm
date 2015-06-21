d328b1e2fbb97dd541fd09ab53725b7cd092f5ce
Rico Suter <mail@rsuter.com>
2015-06-21 04:44:06 +0200

    Updated sample application with custom validation

--- SampleSortableChildCollectionForm/Controllers/PersonController.cs
+++ SampleSortableChildCollectionForm/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using System.Diagnostics;
+using MyToolkit.Html;
 using SampleSortableChildCollectionForm.Models;
 using SampleSortableChildCollectionForm.Models.Editors;
 
@@ -15,11 +16,21 @@ namespace SampleSortableChildCollectionForm.Controllers
         [HttpPost]
         public ActionResult Index(PersonEditViewModel viewModel)
         {
+            // Custom validation
+            foreach (var address in viewModel.Addresses)
+            {
+                if (address.Street.Length < 3)
+                {
+                    var path = CollectionEditorUtilities.GetItemPropertyPath("Addresses", 0, "Street");
+                    ModelState.AddModelError(path, "MyCustomError");
+                }
+            }
+
             if (ModelState.IsValid)
             {
                 // TODO: Save changes of viewModel.Addresses to database
                 Debugger.Break();
-                
+
                 return RedirectToAction("Index");
             }
             else
